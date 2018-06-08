2b0b159cd80984c4544222966ab5e34c07881002
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-06-07 19:23:48 -0700

    adds logic for translation

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
 const svgWidth = 500, svgHeight = 300, barPadding = 5;
-const barWidth = (svgWidth / data.length);
+const barWidth = (svgWidth / dataset.length);
 
-const svg = d3.slect('svg')
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+const svg = d3.select("svg")
+  .attr('height', svgHeight)
+  .attr('width', svgWidth);
+
+
+const barChart = svg.selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append('rect')
+  .attr('y', function(d) { return svgHeight - d; })
+  .attr('height', function(d) { return d; })
+  .attr('width', barWidth - barPadding )
+  .attr('transform', function(d, i) {
+    const translate = [ i * barWidth, 0];
+    return `translate(${translate})`;
+  });
