86898d1062b3878b8e6e2a8e9a7fccbe7a4efd1c
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-06-09 18:47:22 -0700

    adds h and w to svg

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
 const svgWidth = 500, svgHeight = 300, barPadding = 5;
 const barWidth = (svgWidth / data.length);
+
+const svg = d3.slect('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
