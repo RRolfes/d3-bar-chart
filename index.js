4347e31461b15893441df23f54f00c1e82c19bff
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-07-10 20:49:39 -0700

    adds yScale to rects

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+// const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+const dataset = [1,2,3,4,5];
+
 
 const svgWidth = 500, svgHeight = 300, barPadding = 5;
 const barWidth = (svgWidth / dataset.length);
@@ -8,12 +10,18 @@ const svg = d3.select("svg")
   .attr('width', svgWidth);
 
 
+// used to return a number based on proportions to best use space
+const yScaled = d3.scaleLinear()
+  .domain([0, d3.max(dataset) +1])
+  .range([0, svgHeight]);
+
+
 const barChart = svg.selectAll("rect")
   .data(dataset)
   .enter()
   .append('rect')
-  .attr('y', function(d) { return svgHeight - d; })
-  .attr('height', function(d) { return d; })
+  .attr('y', function(d) { return svgHeight - yScaled(d); })
+  .attr('height', function(d) { return yScaled(d); })
   .attr('width', barWidth - barPadding )
   .attr('class', 'bar')
   .attr('transform', function(d, i) {
@@ -21,16 +29,16 @@ const barChart = svg.selectAll("rect")
     return `translate(${translate})`;
   });
 
-  const text = svg.selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .text(function(d){
-      return d;
-    })
-    .attr("y", function(d) {
-      return svgHeight - d - 3;
-    })
-    .attr("x", function(d, i) {
-      return barWidth * i;
-    })
+const text = svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d){
+    return d;
+  })
+  .attr("y", function(d) {
+    return svgHeight - yScaled(d) - 3;
+  })
+  .attr("x", function(d, i) {
+    return barWidth * i + barWidth / 2 - 6;
+  });
