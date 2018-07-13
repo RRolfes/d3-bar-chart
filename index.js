f9284267cee1c9d0fc43bd3d1ce2b67c49966c5d
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-07-13 16:58:00 -0700

    Adds 40

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-const dataset = [1,2,3,4,5,7, 8, 10];
+const dataset = [1,2,3,4,5,7, 8, 10, 20];
 
 
 const svgWidth = 500, svgHeight = 300, barPadding = 5;
