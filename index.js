18654ed517a884115c50e5932e125e77a4d4404e
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-07-21 14:59:52 -0700

    breaks up variables

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 // const dataset = [1,2,3,4,5,7, 8, 10, 20];
 
 
-const svgWidth = 500, svgHeight = 300, barPadding = 5;
+const svgWidth = 500;
+const svgHeight = 300;
+const barPadding = 5;
 const barWidth = (svgWidth / dataset.length);
 
 const svg = d3.select("svg")
